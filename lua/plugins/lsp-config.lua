ad98575b34ba9002961240751f1744be993265e9
Vincent Mingam <vincent.mingam@le64.fr>
2025-09-08 13:23:52 +0200

    Revert "delete lsp-config"
    This reverts commit 522c81d0524f659874c73c89eb28e0fe42323445.

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls" }
+      })
+    end
+  }
+}
